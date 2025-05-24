ebdd67c7d97921973a4549d1074ce38017a07371
giselleandrea <giselle.andrea90@gmail.com>
2025-05-24 16:58:19 -0500

    agregar al carrito ajustes

--- src/main/java/com/condimarket/controllers/OrderController.java
+++ src/main/java/com/condimarket/controllers/OrderController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/api/orders")
 public class OrderController {
 
     private final OrderService orderService;
